192d4465089c7b5998e5549edb8ffdefb2a60073
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-02 10:00:46 +0100

    enumeracja

--- cw_1_pola_figur.py
+++ cw_1_pola_figur.py
@@ -1,4 +1,5 @@
 import math
+from enum import IntEnum
 
 def pole_kwadratu(a):
     return a * a
@@ -12,28 +13,31 @@ def pole_kola(r):
 def pole_trojkata(a, h):
     return 0.5 * a * h
 
+Menu_Figury = IntEnum('Menu_Figury', 'Kwadrat Prostokąt Koło Trójkąt')
 
-wybor = input("""Wybierz figurę, której pole chcesz policzyć:
+print(list(Menu_Figury))
+
+wybor = int(input("""Wybierz figurę, której pole chcesz policzyć:
 1. Kwadrat
 2. Prostokąt
 3. Koło
 4. Trójkat
-""")
+"""))
 
-if wybor == "1":
+if wybor == Menu_Figury.Kwadrat:
     a = float(input("Podaj długość boku kwadratu "))
     wynik = pole_kwadratu(a)
     print("Pole kwadratu to:", wynik)
-elif wybor == "2":
+elif wybor == Menu_Figury.Prostokąt:
     a = float(input("Podaj długość boku prostokąta "))
     b = float(input("Podaj długość boku prostokąta "))
     wynik = pole_prostokata(a, b)
     print("Pole prostokąta to:", wynik)
-elif wybor == "3":
+elif wybor == Menu_Figury.Koło:
     r = float(input("Podaj długość promienia koła "))
     wynik = pole_kola(r)
     print("Pole koła to:",wynik)
-elif wybor == "4":
+elif wybor == Menu_Figury.Trójkąt:
     a = float(input("Podaj długość podstawy trójkąta "))
     h = float(input("Podaj wysokość trójkąta "))
     wynik = pole_trojkata(a, h)
