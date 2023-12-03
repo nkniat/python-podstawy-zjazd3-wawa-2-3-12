300f41e89ae78f2aca1aba55768c80971c961720
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-03 10:43:22 +0100

    wyjatki - dodatkowe informacje

--- cw12_wyjatki.py
+++ cw12_wyjatki.py
@@ -11,8 +11,23 @@ else:
 finally:
     print("Koniec operacji")
 
+licznik = 0
+while True:
+    try:
+        x = int(input("Wprowadź liczbę całkowitą "))
+        break
+    except ValueError:
+        # raise ValueError('Coś poszło nie tak')
 
-try:
-    x = int(input("Wprowadź liczbę całkowitą "))
-except ValueError:
-    print("Usik, to nie jest liczb całkowita")
+        # try:
+        #     x = float(x)
+        #     print("Nie udało się zapisać jako int, jest jako float")
+        # except ValueError:
+        #     print("nie udało się zapisać inta jako float")
+        print("Usik, to nie jest liczba całkowita")
+        licznik += 1
+        if licznik == 3:
+            print("Wykorzystałeś 3 próby.")
+            break
+
+print("Dalsza część kodu")
