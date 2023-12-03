fa2573dbe21e711857881666e7492f121f41877d
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-03 09:08:53 +0100

    *args i **kwargs

---
+++ cw10_args_kwargs.py
@@ -0,0 +1,37 @@
+# podejście klasyczne
+def dodawanie(a, b):
+    return a + b
+
+def dodawanie_3_liczb(a, b, c):
+    return a + b
+
+def dodawanie_4_liczb(a, b, c, d):
+    return a + b
+
+# wykorzystanie *args - nieznana liczba argumentów
+# argumenty przekazywane do krotki
+def dodawanie_liczb(*liczby):
+    wynik = 0
+    for liczba in liczby:
+        wynik += liczba
+    return wynik
+
+
+# **args - nieznana liczba argumentów
+# argumenty przekazywane wformie klucz-wartość (słownik)
+def zrobZdanie(**slowa):
+    zdanie = ''
+    for slowo in slowa.values():
+        zdanie += slowo
+        zdanie += ' '
+    return zdanie
+# zadanie domwe: zmodyfikuj funkcje o 'bajery', np. Pierwsze słowo musi być z wielkiej litery, zdanie ma sie kończyć kropką
+# albo spytaj użytkownika, jak ma się kończyć zdanie?
+
+print("Dodawanie wielu liczb\n wynik to: ", dodawanie_liczb(1, 2))
+print("Dodawanie wielu liczb\n wynik to: ", dodawanie_liczb(1, 2, 3))
+print("Dodawanie wielu liczb\n wynik to: ", dodawanie_liczb(1, 2, 3, 4))
+print("Dodawanie wielu liczb\n wynik to: ", dodawanie_liczb(1, 2, 3, 4, 5.5))
+
+print("Nowe zdanie to: ", zrobZdanie(slowo1='Python', slowo2='jest', slowo3='fajny'))
+
